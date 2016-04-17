a9935cea919aea7fce647a2c1279d6c981f626c4
Dann Blair <dann@bitspacedevelopment.com>
2016-04-16 22:33:07 -0500

    :rocket: here we go

---
+++ server.js
@@ -0,0 +1,7 @@
+// Super simple server script for local dev
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(9090, function(){
+    console.log('Server running on 9090...');
+});
